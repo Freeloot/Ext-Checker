b9df21e5a6932411a3f3195e45b9129aa3871891
Infamy <65705916+Freeloot@users.noreply.github.com>
2022-05-10 11:03:11 +0100

    Update ext.py

--- src/ext.py
+++ src/ext.py
@@ -3,8 +3,8 @@ import sys, pathlib, os
 def main(fpath):
 	if os.path.isfile(fpath):
 		file_ext = pathlib.Path(fpath).suffix 
-		print(file_ext)
+		return file_ext
 	else:
 		exit()
 
-if __name__ == '__main__': main(sys.argv[1])
+if __name__ == '__main__': print(main(sys.argv[1]))
